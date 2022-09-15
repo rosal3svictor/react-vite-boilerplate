ab130823efee9c867a5e9bd93f96696369efc4ab
Victor Rosales <rosalesvictor.dev@gmail.com>
2022-09-15 07:21:57 -0500

    chore: Introduce .eslint configuration

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
+import { Button } from './Button'
+import './header.css'
 
-import { Button } from './Button';
-import './header.css';
-
-type User = {
-  name: string;
-};
-
-interface HeaderProps {
-  user?: User;
-  onLogin: () => void;
-  onLogout: () => void;
-  onCreateAccount: () => void;
+interface User {
+  name: string
 }
 
-export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
+interface HeaderProps {
+  user?: User
+  onLogin: () => void
+  onLogout: () => void
+  onCreateAccount: () => void
+}
+
+export const Header: React.FC<HeaderProps> = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <header>
     <div className="wrapper">
       <div>
@@ -37,20 +35,22 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
         <h1>Acme</h1>
       </div>
       <div>
-        {user ? (
+        {(user != null)
+          ? (
           <>
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
             <Button size="small" onClick={onLogout} label="Log out" />
           </>
-        ) : (
+            )
+          : (
           <>
             <Button size="small" onClick={onLogin} label="Log in" />
             <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
           </>
-        )}
+            )}
       </div>
     </div>
   </header>
-);
+)
